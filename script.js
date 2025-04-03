47186f1cedd27bb52f8abc19f6800054d02d09fa
rcv23pp <143848608+rcv23pp@users.noreply.github.com>
2025-04-03 19:16:53 -0300

    jw

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
-
-// Aguarda atÃ© que todo o conteÃºdo da pÃ¡gina (HTML, CSS, etc.) esteja carregado
-
 document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    
-    const botaoDeAcessibilidade = document.getElementsByClassName('botao-acessibilidade');
-    const opcoesDeAcessibilidade = document.getElementsByClassName('apresenta-lista');
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-    botaoDeAcessibilidade.addEventListener('click' , fuction () {
-        botaoDeAcessibilidade.classlist.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.claslist.togg('apresenta-lista')
     })
-
-
-    // Acessa o botÃ£o com o ID 'aumentar-fonte' no HTML e o armazena em uma variÃ¡vel
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    // Inicializa uma variÃ¡vel que controla o tamanho da fonte. ComeÃ§a com 1rem.
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
 
-    // Adiciona um evento de clique ao botÃ£o 'aumentar-fonte'
-
     aumentaFonteBotao.addEventListener('click', function () {
-
-        // Quando o botÃ£o for clicado, aumenta o valor de tamanhoAtualFonte em 0.1
-
         tamanhoAtualFonte += 0.1;
-
-        // Atualiza o estilo da fonte do corpo da pÃ¡gina (document.body), alterando seu tamanho para o valor de 'tamanhoAtualFonte'
-
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
 
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-});
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
+})
